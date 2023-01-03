912c36724b6de7019cc929f57ff40a81e1d92b5f
tara-lecoz <tara.le-coz@laplateforme.io>
2023-01-03 14:56:13 +0100

    Modifications triangle isocèle et rectangle

--- jour2/pourallerplusloin/pourallerplusloin.py
+++ jour2/pourallerplusloin/pourallerplusloin.py
@@ -1,11 +1,15 @@
+import math
 def triangleoupas(a,b,c):
     if a + b >= c:
         result=print("C'est un triangle existant")
         if a == b == c:
             result=print("Il est équilatéral")
         elif a == b or b == c or c == a:
-            result=print("Il est isocèle")
-        elif a^2+b^2 == c^2:
+            if a**2+b**2 == int(c**2):
+                result=print("Il est isocèle et rectangle")
+            else:
+                print("Il est isocèle")
+        elif a**2+b**2 == c**2:
             result=print("Il est rectangle")
     else:
         result=print("Ce n'est pas un triangle")
@@ -22,3 +26,5 @@ triangleoupas(3,3,3)
 print("---------------------")
 triangleoupas(1,1,12)
 print("---------------------")
+triangleoupas(1,1,math.sqrt(2))
+
